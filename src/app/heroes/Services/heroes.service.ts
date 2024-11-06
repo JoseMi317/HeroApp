9ffa596762c971cf15d334420a2922341d6a3493
Jose Miguel <jmqc77@gmail,com>
2024-11-06 12:15:49 -0600

    First commit

---
+++ src/app/heroes/Services/heroes.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
+import { Hero } from '../Interfaces/hero.interface';
+import { environments } from '../../../environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+
+  private baseUrl : string = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes():Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  getHerobyId(id: string) : Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${this.baseUrl} /heroes/${id}`)
+      .pipe(
+        catchError( error => of(undefined))
+      )
+
+  }
+
+}
